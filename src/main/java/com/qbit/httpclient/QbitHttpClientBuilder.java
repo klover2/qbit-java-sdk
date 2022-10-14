35cc02116db01c68c741559ce3ead65b44a8e51a
klover <49420645@qq.com>
2022-10-15 00:38:18 +0800

    fix

--- src/main/java/com/qbit/httpclient/QbitHttpClientBuilder.java
+++ src/main/java/com/qbit/httpclient/QbitHttpClientBuilder.java
@@ -9,9 +9,6 @@ import org.apache.http.impl.client.HttpClientBuilder;
  * date 2022/10/13 18:23
  */
 public class QbitHttpClientBuilder extends HttpClientBuilder {
-    private static final String OS = System.getProperty("os.name") + "/" + System.getProperty("os.version");
-    private static final String VERSION = System.getProperty("java.version");
-
     private QbitHttpClientBuilder() {
         super();
     }
